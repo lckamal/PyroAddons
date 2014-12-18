1262e9f0427e15f2b61c4f652648600236d2d883
lckamal <lckamal@hotmail.com>
2014-12-18 11:22:31 +0545

    imagepicker module - merged field type to imagepicker module folder

--- modules/imagepicker/details.php
+++ modules/imagepicker/details.php
@@ -4,6 +4,30 @@
 
 	public $version  = '0.1';
 
+	public function __construct()
+    {
+        parent::__construct();
+
+        @set_time_limit(60);
+
+        // Load in the FireSale library
+        $this->lang->load('imagepicker/imagepicker');
+        $this->load->library('streams_core/type');
+
+        // Add our field type path
+        $core_path = defined('PYROPATH') ? PYROPATH : APPPATH;
+
+        if (is_dir(SHARED_ADDONPATH.'modules/imagepicker/field_types')) {
+            $this->type->addon_paths['imagepicker'] = SHARED_ADDONPATH.'modules/imagepicker/field_types/';
+        } elseif (is_dir($core_path.'modules/imagepicker/field_types')) {
+            $this->type->addon_paths['imagepicker'] = $core_path.'modules/imagepicker/field_types/';
+        } else {
+            $this->type->addon_paths['imagepicker'] = ADDONPATH.'modules/imagepicker/field_types/';
+        }
+
+        $this->type->gather_types();
+    }
+
 	public function info()
 	{
 		return array(
